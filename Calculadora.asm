1d5af7b02af4301e56cf1f285a0f00d30ccb7c06
LiliaLobato <ie706937@iteso.mx>
2018-09-05 12:23:38 -0500

    Calculadora Terminada

--- Calculadora.asm
+++ Calculadora.asm
@@ -29,12 +29,20 @@ case3:
 
 	#Funciones
 andBitwise:
+	add $s3,$zero,$zero
+	and $s3, $s1, $s2
 	jr $ra 			#Returning to Switch Case
 suma:
+	add $s3,$zero,$zero
+	add $s3,$s1, $s2
 	jr $ra 			#Returning to Switch Case
 resta:
+	add $s3,$zero,$zero
+	sub $s3,$s1,$s2
 	jr $ra 			#Returning to Switch Case
 multiplica:
+	add $s3,$zero,$zero
+	mul $s3, $s1, $s2
 	jr $ra 			#Returning to Switch Case
 
 
