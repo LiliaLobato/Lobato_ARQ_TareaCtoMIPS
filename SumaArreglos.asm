4f90788e90a08a3d7617203b66bfae9635350706
LiliaLobato <ie706937@iteso.mx>
2018-09-07 01:16:05 -0500

    SumaArreglos Terminado

---
+++ SumaArreglos.asm
@@ -0,0 +1,29 @@
+# Author: Lilia Lobato
+# Date: Sep 05, 2018
+
+.data
+	#Valor de Arreglos
+	Vector1: .word 1 2 3 4 5 6 7 8 9   	 # Vector1
+.text
+	# Almacena la direccion de cada Arreglo
+	la $s1, Vector1
+	
+	#Carga Valores
+	add $s4,$zero,$zero	#i
+	add $s5,$zero,$zero	#resultado
+	add $s6, $s6, 9		#tamaño arreglos
+	
+	#For
+for:	
+	jal ProductFunction
+	add $s4,$s4, 0x1	#Aumento en variable i por ciclo
+	bne $s4, $s6, for	#salto for
+	j exit
+	
+ProductFunction:
+	lw $t0, ($s1) 		#Loading data from memory
+	add $s5, $s5, $t0	#suma de valores
+	add $s1, $s1, 4		#se mueve a la siguiente localidad de memoria
+	jr $ra 			#Returning to Switch Case
+
+exit:
